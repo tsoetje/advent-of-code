470e6aa9fd2b372b02abfed9f46c275b90815664
Thimo <thimo.soet54@gmail.com>
2021-12-01 11:33:38 +0100

    part one finished

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -4,16 +4,24 @@ import readInput
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val seaFloorDepth = input.map { it.toInt() }
+
+        var depthIncreased = 0
+        seaFloorDepth.forEachIndexed { index, depth ->
+            if(index == 0) return@forEachIndexed
+
+            val previousDepth = seaFloorDepth[index - 1]
+            if (depth > previousDepth) {
+                depthIncreased++
+            }
+        }
+
+        return depthIncreased
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("day01/Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("day01/Day01")
     println(part1(input))
