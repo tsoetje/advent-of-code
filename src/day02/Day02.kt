f257be06ff36c1ae02efa35060d13c567575f67d
Thimo <thimo.soet54@gmail.com>
2021-12-02 10:03:52 +0100

    part 2 done

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -23,5 +23,29 @@ fun main() {
         }
         return forward * vertical
     }
+
+    fun part02(input: List<String>): Int{
+        var horizontal = 0
+        var vertical = 0
+        var aim = 0
+
+        for (string in input) {
+            val (direction, amountString) = string.split(" ")
+            val amount = amountString.toInt()
+
+            when (direction) {
+                "up" -> aim -= amount
+                "down" -> aim += amount
+                "forward" -> {
+                    horizontal += amount
+                    vertical += (aim * amount)
+                }
+            }
+
+        }
+        return vertical * horizontal
+    }
+
     println(part01(input))
+    println(part02(input))
 }
